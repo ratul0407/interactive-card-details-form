63ceb622d1855433123524868cac4cce08c4c22a
Ratul <rajaulislamratul12@gmail.com>
2023-11-30 21:18:21 +0600

    ⚙ added the form validation

--- script.js
+++ script.js
@@ -22,6 +22,10 @@ const submitBtnEl = document.querySelector("button");
 
 //all input fields
 const allInputEl = document.querySelectorAll("input");
+
+//form element
+const form = document.querySelector(".form_container");
+console.log(form);
 //adding the interactivity
 
 let interactiveElements = [...interactiveEl, cvcEl];
@@ -68,6 +72,18 @@ dateCvcEl.forEach((el, i) => {
 
 submitBtnEl.addEventListener("click", function (e) {
   e.preventDefault();
+  check();
+  let done = 0;
+  allInputEl.forEach((el, i) => {
+    if (!el.classList.contains("error")) {
+      el.style.borderColor = "teal";
+      done++;
+    }
+  });
+  if (done === 5) {
+    form.className = "hidden";
+    document.querySelector(".complete").classList.remove("hidden");
+  }
 });
 
 const maxNum = function (element) {
@@ -88,3 +104,62 @@ function enforceMinMax(el) {
     }
   }
 }
+
+function check() {
+  const name = cardHolderInputEl.value.trim();
+  const cardNumber = cardNumberInputEl.value.trim();
+  const month = dateCvcEl[0].value.trim();
+  const year = dateCvcEl[1].value.trim();
+  const cvc = dateCvcEl[2].value.trim();
+  const allElements = [name, cardNumber, month, year, cvc];
+  const valid = [];
+  console.log(allElements);
+  //checking all the elements if there is any blank one
+  allElements.forEach((el, i) => {
+    setErrorMsg(el, errorMsgEl[i], allInputEl[i]);
+    if (setErrorMsg(el, errorMsgEl[i], allInputEl[i]))
+      allInputEl[i].classList.remove("error");
+    errorMsgEl[i].textContent = "";
+    if (i >= 1) checkLenght(allInputEl[i], errorMsgEl[i], allInputEl[i]);
+    console.log(Boolean(setErrorMsg(el, errorMsgEl[i], allInputEl[i])));
+  });
+  //checking for any number on name element
+
+  onlyLetters(name, errorMsgEl[0], allInputEl[0]);
+  //Checking if there is any alphabets in the second input element (Card number)
+  onlyNumbers(cardNumber, errorMsgEl[1], allInputEl[1]);
+
+  function setErrorMsg(el, errorEl, inputEl) {
+    if (!el) {
+      errorEl.textContent = "can't be blank";
+      inputEl.classList.add("error");
+      return true;
+    }
+    return valid.push(inputEl);
+  }
+
+  function onlyNumbers(el, errorEl, inputEl) {
+    if (el.value && Number(el.value.trim()) !== NaN) {
+      errorEl.textContent = "Only numbers";
+      inputEl.classList.add("error");
+      return true;
+    }
+    return valid.push(inputEl);
+  }
+  function onlyLetters(el, errorEl, inputEl) {
+    if (/\d/.test(el)) {
+      errorEl.textContent = "Only alphabets";
+      inputEl.classList.add("error");
+      return true;
+    }
+    return valid.push(inputEl);
+  }
+  function checkLenght(el, errorEl) {
+    if (el.maxLength > el.value && el.value) {
+      errorEl.textContent = `invalid(too short)`;
+      return true;
+    }
+    return valid.push(el);
+  }
+}
+console.log();
